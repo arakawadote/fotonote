e3eda47abe126bc21b22f1a05f9744ac58046329
arakawadote <12594387+arakawadote@users.noreply.github.com>
2026-05-13 13:57:05 +0900

    Hide missing focal length metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.arakawadote.fotonote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.2"
+        versionCode = 5
+        versionName = "0.2.3"
     }
 
     signingConfigs {
